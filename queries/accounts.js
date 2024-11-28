f01c551e45e094f4bf88c8452fa4f9e9f35ff65d
arim83 <jorammercado@pursuit.org>
2024-11-28 06:36:32 -0500

    Create account query added

--- queries/accounts.js
+++ queries/accounts.js
@@ -41,9 +41,35 @@ const getOneAccountByUserName = async ( username ) => {
     }
 }
 
+const createAccount = async (user) => {
+    try {
+        const createdUser = await db.one(`INSERT INTO accounts (firstname,` +
+            ` lastname,` +
+            ` email,` +
+            ` password_hashed,` +
+            ` username,` +
+            ` profile_img,` +
+            ` about,` +
+            ` dob) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *`,
+            [user.firstname,
+            user.lastname,
+            user.email,
+            user.password,
+            user.username,
+            user.profile_img,
+            user.about,
+            user.dob])
+        return createdUser
+    }
+    catch (err) {
+        return { err: `${err}, sql query error - create user` }
+    }
+}
+
 module.exports = {
     getOneAccount,
     getAllAccounts,
     getOneAccountByEmail,
-    getOneAccountByUserName
+    getOneAccountByUserName,
+    createAccount
 }
