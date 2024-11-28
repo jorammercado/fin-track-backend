ccaf8a3f5b753f9aa44d3e794423cb263d90aeb7
arim83 <jorammercado@pursuit.org>
2024-11-28 13:20:07 -0500

    check username exists validation addded

--- validations/checkAccount.js
+++ validations/checkAccount.js
@@ -1,3 +1,5 @@
+const { getOneAccountByUserName } = require("../queries/accounts")
+
 const checkUsernameProvided = (req, res, next) => {
     if (req.body?.username) {
         return next()
@@ -6,6 +8,16 @@ const checkUsernameProvided = (req, res, next) => {
     }
 }
 
+const checkUsernameExists = async (req, res, next) => {
+    const registeredAccount = await getOneAccountByUserName(req.body?.username)
+    if (registeredAccount) {
+        res.status(400).json({ error: "Account already registered with this username" })
+    } else {
+        next()
+    }
+}
+
 module.exports = {
-    checkUsernameProvided
+    checkUsernameProvided,
+    checkUsernameExists
 }
