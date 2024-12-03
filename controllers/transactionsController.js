a0062b240e69ef468ec4248bd793aab284362701
arim83 <jorammercado@pursuit.org>
2024-12-03 00:34:11 -0500

    get all transactions route added

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -18,6 +18,25 @@ const { checkAmountProvided,
 } = require("../validations/checkTransactions.js")
 
 // get all
+transactions.get("/", checkAccountID, async (req, res) => {
+    const { account_id } = req.params
+    try {
+        const transactionsList = await getAllTransactions(account_id)
+
+        if (transactionsList.err) {
+            return res.status(500).json({ error: transactionsList.err })
+        }
+
+        if (transactionsList.length === 0) {
+            return res.status(200).json([]) 
+        }
+
+        return res.status(200).json(transactionsList)
+    } catch (error) {
+        console.error("Error fetching transactions:", error)
+        return res.status(500).json({ error: "Internal server error" })
+    }
+})
 
 // get one
 
