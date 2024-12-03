00622ca01b73c2278e7c237439f9864be14f1cec
arim83 <jorammercado@pursuit.org>
2024-12-02 20:29:24 -0500

    get one transaction query added

--- queries/transactions.js
+++ queries/transactions.js
@@ -11,6 +11,18 @@ const getAllTransactions = async (account_id) => {
     }
 }
 
+const getOneTransaction = async (transaction_id) => {
+    try {
+        const oneTransaction = await db.oneOrNone(`SELECT * FROM financial_transactions WHERE transaction_id=$1`,
+            [transaction_id])
+        return oneTransaction
+    }
+    catch (err) {
+        return { err: `${err}, sql query error - get one transaction` }
+    }
+}
+
 module.exports = {
-    getAllTransactions
+    getAllTransactions,
+    getOneTransaction
 }
