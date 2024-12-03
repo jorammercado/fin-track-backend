2280ddd8dc532751aae47c64db8429d0ec0cee87
arim83 <jorammercado@pursuit.org>
2024-12-02 22:51:17 -0500

    check transaction type validation func. added

--- validations/checkTransactions.js
+++ validations/checkTransactions.js
@@ -7,14 +7,31 @@ const checkAmountProvided = (req, res, next) => {
         return res.status(400).json({ error: "Amount is required!" })
     }
 
-    if (typeof amount !== 'number' ) {
+    if (typeof amount !== 'number') {
         return res.status(400).json({ error: "Amount must be a number." })
     }
 
     return next()
 }
 
+const checkTransactionTypeProvided = (req, res, next) => {
+    const { transaction_type } = req.body
+
+    if (!transaction_type) {
+        return res.status(400).json({ error: "Transaction type is required!" })
+    }
+
+    const allowedTypes = ['income', 'expense', 'investment']
+    if (!allowedTypes.includes(transaction_type)) {
+        return res.status(400).json({ error: `Invalid transaction type: ${transaction_type}. Allowed values are: ${allowedTypes.join(', ')}` })
+    }
+
+    return next()
+}
+
+
 
 module.exports = {
-    checkAmountProvided
+    checkAmountProvided,
+    checkTransactionTypeProvided
 }
